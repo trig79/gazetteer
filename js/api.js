09eef8fce4687a031437fc475de1fb16b64fc4d8
Martin Durrant <65402006+trig79@users.noreply.github.com>
2021-01-14 19:58:07 +0000

    minor

--- js/api.js
+++ js/api.js
@@ -1,10 +1,23 @@
 //On Load uses geolocation to call API's:  currentTime | {OpenCage > RestCountries > OpenWeather}
-window.addEventListener('load', () => {
+window.addEventListener('load', e => {
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(navSuccess, navError)
     }       
+
 })
+
+async function registerSW() { 
+    if ('serviceWorker' in navigator) { 
+      try {
+        await navigator.serviceWorker.register('./sw.js'); 
+      } catch (e) {
+        alert('ServiceWorker registration failed. Sorry about that.'); 
+      }
+    } else {
+      document.querySelector('.alert').removeAttribute('hidden'); 
+    }
+  }
 
 const navSuccess = (position) => {
     let lon = position.coords.longitude.toFixed(2);
